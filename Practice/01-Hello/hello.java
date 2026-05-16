a928b8ec1698d4476feb7ecec399a765bcd752b5
freewindias <freewindias6@gmail.com>
2026-05-16 12:56:43 -0700

    2

---
+++ Practice/01-Hello/hello.java
@@ -0,0 +1,11 @@
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello, Im Freewin Dias, 22 years old from Mumbai, Maharashtra");
+        System.out.println("I am a student, developer & learner");
+        System.out.println("I am passionate about technology, innovation & problem solving");
+        System.out.println("I am a curious individual with a never give up attitude");
+        System.out.println("I am a team player & a quick learner");
+        System.out.println("Looking forward to create something meaningful & impactful");
+    }
+}
---
+++ Practice/02-Variables/variables.java
@@ -0,0 +1,15 @@
+public class variables {
+
+    public static void main(String[] args) {
+        String name = "Freewin Dias";
+        int age = 23;
+        double height = 5.5;
+        boolean isMale = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is Male: " + isMale);
+
+    }
+}
