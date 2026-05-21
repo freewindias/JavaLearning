bd69d5aee154d168cbc848ad2dd248654aca61cc
freewindias <freewindias6@gmail.com>
2026-05-20 17:14:56 -0700

    3

---
+++ Practice/02-Variables/carVariables.java
@@ -0,0 +1,18 @@
+public class carVariables {
+
+    public static void main(String[] args) {
+        String make = "Ford";
+        String model = "Mustang";
+        int year = 2024;
+        double mileage = 15002.5;
+        boolean isAutomatic = true;
+
+        System.out.println("Car Make: " + make);
+        System.out.println("Car Model: " + model);
+        System.out.println("Car year: " + year);
+        System.out.println("Car Mileage: " + mileage + " miles driven.");
+        System.out.println("Car Automatic: " + isAutomatic);
+
+
+    }
+}
